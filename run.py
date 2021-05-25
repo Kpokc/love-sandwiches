6c387fdf7a630a0a70875dddef44854de0236b8e
Kpokc <pavmakarovs@gmail.com>
2021-05-25 21:06:42 +0000

    Collect sales data for last 5 rows

--- run.py
+++ run.py
@@ -77,15 +77,26 @@ def calculate_surplus_data(sales_row):
     
     return surplus_data
 
+def get_las_5_entries_sales():
+    sales = SHEET.worksheet("sales")
+    columns = []
+    for ind in range(1,7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    
+    return columns
+
 def main():
     """
     Run all program functions
-    """
+    
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_worksheet(sales_data, "sales")
     surplus_result = calculate_surplus_data(sales_data)
     update_worksheet(surplus_result, "surplus")
+    """
+    sales_columns = get_las_5_entries_sales()
 
 print("Welcome to Love  Sandwiches data automation\n")
 main()
